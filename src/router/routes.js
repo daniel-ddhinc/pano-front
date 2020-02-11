c675768e445db2bf600f57b0f5a261afea527273
daniel <daniel@ddhinc.net>
2020-02-11 10:10:26 +0900

    routes signin, signup, /

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,12 +5,55 @@ import DefaultLayout from "@/layouts/Default";
 
 const publicRoute = [
   {
+    path: "/",
+    component: AuthLayout,
+    meta: { title: "public" },
+    redirect: "/login",
+    children: [
+      {
+        path: "/login",
+        name: "login",
+        meta: { title: "login" },
+        component: () => import("@/views/auth/Login.vue")
+      }
+    ]
+  },
+  {
+    path: "/",
+    component: AuthLayout,
+    meta: { title: "register" },
+    redirect: "/register",
+    children: [
+      {
+        path: "/register",
+        name: "register",
+        meta: { title: "register" },
+        component: () => import("@/views/auth/Register")
+      }
+    ]
+  },
+  {
     path: "*",
     redirect: "/dashboard"
   }
 ];
 
-const protectedRoute = [];
+const protectedRoute = [
+  {
+    path: "/",
+    component: DefaultLayout,
+    meta: { title: "public" },
+    redirect: "/",
+    children: [
+      {
+        path: "/",
+        name: "home",
+        meta: { title: "home" },
+        component: () => import("@/views/main/Home.vue")
+      }
+    ]
+  }
+];
 
 const routes = protectedRoute.concat(publicRoute);
 export default routes;
