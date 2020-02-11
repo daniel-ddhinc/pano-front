8666b1e95be80012795b89d9de5c2df4f7dc0431
daniel <daniel@ddhinc.net>
2020-02-11 10:36:15 +0900

    vuex icon plugin

---
+++ src/plugins/icons.js
@@ -0,0 +1,13 @@
+import caries from "@/assets/icon_caries.svg";
+import cariesActive from "@/assets/icon_caries_active.svg";
+import plesion from "@/assets/icon_plesion.svg";
+import plesionActive from "@/assets/icon_plesion_active.svg";
+
+const MY_ICONS = {
+  caries,
+  cariesActive,
+  plesion,
+  plesionActive
+};
+
+export default MY_ICONS;
