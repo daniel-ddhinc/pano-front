01812a3b36d42ab0c35bffac32205e4a782782f7
daniel <daniel@ddhinc.net>
2020-02-11 09:57:09 +0900

    vue config setup for proxy, port

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,22 @@
+const path = require("path");
+
 module.exports = {
+  devServer: {
+    proxy: {
+      "/api": {
+        target: process.env.VUE_APP_ROOT_API,
+        changeOrigin: true,
+        secure: false
+      }
+    },
+    port: 8080
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": path.join(__dirname, "src/")
+      }
+    }
+  },
   transpileDependencies: ["vuetify"]
 };
