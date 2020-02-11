0ff0a090016d8d2ad1fc5122fdc713d6b9c2a114
daniel <daniel@ddhinc.net>
2020-02-11 10:38:35 +0900

    #4 verify token

---
+++ src/views/auth/Verification.vue
@@ -0,0 +1,15 @@
+<template>
+  <div />
+</template>
+
+<script>
+import { verificationAPI } from "@/api/auth";
+export default {
+  async mounted() {
+    const { token } = this.$route.params;
+    const { data } = await verificationAPI(token);
+    data && alert(data);
+    this.$router.replace("/login");
+  }
+};
+</script>
